5ca5c97567d6d5753d02a8d00ee9a3fb90d2b0c6
KrackerOs <kleberisaac900@gmail.com>
2025-09-03 11:40:19 -0300

    HFHFH

---
+++ nizuC/src/DIA999/aula11.java
@@ -0,0 +1,18 @@
+package App2;
+import  java.util.Scanner;
+public class aula11{
+
+    //while = enquanto
+    public static void main(String[] args) {      
+        //int contador = 1;
+
+        //while (contador <=50) {
+           // System.out.println(contador);
+            //contador ++ ;//gera condição falsa
+            //incremento
+       // }
+
+       Scanner entrada = new Scanner(System.in);
+       String nome;
+       System.out.println("Digite o seu nome ou s para sair: ");
+       nome = entrada.nextLine();
---
+++ nizuC/src/aula/App.java
@@ -0,0 +1,12 @@
+package aula;
+
+public class App{
+
+String nome;
+String descricao;
+String autor;
+String isbn;
+double preco;
+String datapub;
+
+}
